88afaf767861de938c8a1a6f7d780aa9b9f262fb
Niko Perry <perry.niko7@gmail.com>
2021-07-29 18:17:19 -0700

    Initial Dice Game

---
+++ dice.py
@@ -0,0 +1,87 @@
+import random
+import time
+
+#logic for visualization of dice
+def dice(a):
+    if (a==1):
+        print('|       |')
+        print('|       |')
+        print('|   •   |')
+        print('|       |')
+        print('|       |\n')
+
+    elif (a==2):
+        print('|       |')
+        print('|  •    |')
+        print('|       |')
+        print('|    •  |')
+        print('|       |\n')
+
+    elif (a==3):
+        print('|       |')
+        print('|  •    |')
+        print('|   •   |')
+        print('|    •  |')
+        print('|       |\n')
+
+    elif (a==4):
+        print('|        |')
+        print('|  •  •  |')
+        print('|        |')
+        print('|  •  •  |')
+        print('|        |\n')
+
+
+    elif (a==5):
+        print('|       |')
+        print('|  • •  |')
+        print('|   •   |')
+        print('|  • •  |')
+        print('|       |\n')
+
+    elif (a==6):
+        print('|        |')
+        print('|  •  •  |')
+        print('|  •  •  |')
+        print('|  •  •  |')
+        print('|        |\n')
+
+#logic for rolling the dice
+def roll():
+    player = random.randint(1,6)
+    print('You rolled')
+    time.sleep(1)
+    print('You rolled a', player)
+    dice(player)
+    print('')
+
+    ai = random.randint(1,6)
+    print('The computer rolls...')
+    time.sleep(2)
+    print('The computer rolled a', ai)
+    dice(ai)
+    print('')
+
+    if player > ai:
+        print('You win.')
+    elif player == ai:
+        print('Tie game.')
+    else:
+        print('You lose.')
+
+    print('---------------\n')
+
+#GUI
+while True:
+    print("Welcome to Niko's Dice Simulator!")
+    print('Enjoy!')
+    print()
+    print()
+    print("Press 'r' to roll your die.")
+    print("Enter 'q' to end the program")
+    user_input = input()
+
+    if user_input == "q":
+        break
+    else:
+        roll()
